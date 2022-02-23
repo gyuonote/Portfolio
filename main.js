e55ecd82d429d9d9bd6f5650953f1771e28ce967
Gyuo Sim <77821271+gyuonote@users.noreply.github.com>
2022-02-23 19:45:54 +0900

    JS: typing animation

--- main.js
+++ main.js
@@ -29,6 +29,28 @@ navbarToggleBtn.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
+let target = document.querySelector("#dynamic");
+let stringArr = ["안녕하세요 Front-end developer심규오입니다."];
+let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+let selectStringArr = selectString.split("");
+
+// 한글자씩 텍스트 출력 함수
+function dynamic(randomArr) {
+  if (randomArr.length > 0) {
+    target.textContent += randomArr.shift();
+    setTimeout(function () {
+      dynamic(randomArr);
+    }, 80);
+  }
+}
+dynamic(selectStringArr);
+
+//커서 깜빡임 효과
+function blink() {
+  target.classList.toggle("active");
+}
+setInterval(blink, 500);
+
 // Handle click on "contact me" button on home
 
 const homeContactBtn = document.querySelector(".home__contact");
@@ -79,7 +101,6 @@ workBtnContainer.addEventListener("click", (e) => {
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project.dataset.type);
       if (filter === "*" || project.dataset.type.includes(filter)) {
         project.classList.remove("invisible");
       } else {
@@ -90,7 +111,62 @@ workBtnContainer.addEventListener("click", (e) => {
   }, 300);
 });
 
+// 1. 모든 섹션 요소들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = ["#home", "#about", "#skills", "#work", "#contact"];
+
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
+}
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      let selectedIndex;
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener("wheel", () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    window.scrollY + window.innerHeight ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
