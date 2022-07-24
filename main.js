2ca0eb654d936c6862e10f8fa26aab4079c0ed15
simgyuo <gosim8611@gmail.com>
2022-07-24 22:12:44 +0900

    JS-project-Shopping

--- main.js
+++ main.js
@@ -171,3 +171,63 @@ window.addEventListener("wheel", () => {
   }
   selectNavItem(navItems[selectedNavIndex]);
 });
+
+// Project Shopping List
+
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
+
+function onAdd() {
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+
+  items.appendChild(item);
+
+  input.value = "";
+  input.focus();
+
+  function createItem(text) {
+    const itemRow = document.createElement("li");
+    itemRow.setAttribute("class", "item__row");
+
+    const item = document.createElement("div");
+    item.setAttribute("class", "item");
+
+    const itemName = document.createElement("span");
+    itemName.setAttribute("class", "item__name");
+    itemName.innerHTML = text;
+
+    const itemDivider = document.createElement("div");
+    itemDivider.setAttribute("class", "item__divider");
+
+    const itemDelete = document.createElement("button");
+    itemDelete.setAttribute("class", "item__delete");
+    itemDelete.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    itemDelete.addEventListener("click", () => {
+      items.removeChild(itemRow);
+      input.focus();
+    });
+
+    itemRow.appendChild(item);
+    itemRow.append(itemDivider);
+    item.appendChild(itemName);
+    item.appendChild(itemDelete);
+    return itemRow;
+  }
+}
+
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    onAdd();
+  }
+});
